649474453ef6c1bd497f77532920f3deeec9ef31
Dan Bernier <dbernier@continuity.net>
2016-01-13 21:48:28 +0000

    Make all requests via POST, not GET.
    The NCUA just changed their endpoint.

--- lib/ncua/client.rb
+++ lib/ncua/client.rb
@@ -7,20 +7,21 @@ module NCUA
     #debug_output $stderr
 
     def find_credit_union_by_address(address, radius = 100)
-      self.class.get(query_endpoint, query: {
+      self.class.post(query_endpoint, query: {
         address: address,
         type: 'address',
         radius: radius.to_s })
     end
 
     def find_credit_union_by_name(name)
-      self.class.get(query_endpoint, query: {
+      self.class.post(query_endpoint, query: {
         address: name,
         type: 'cuname' })
     end
 
     def find_credit_union_by_charter_number(charter_number)
-      self.class.get(query_endpoint, query: {
+      self.class.post(query_endpoint, query: {
+        radius: 100,
         address: charter_number,
         type: 'cunumber' })
     end
