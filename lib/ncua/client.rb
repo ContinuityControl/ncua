e8790ff9f68a34d4f1fd1684aed8839783089d25
Tom Reznick <treznick@continuty.net>
2018-04-25 15:17:01 +0000

    Handle empty string being sent as json...

--- lib/ncua/client.rb
+++ lib/ncua/client.rb
@@ -7,29 +7,35 @@ module NCUA
     #debug_output $stderr
 
     def find_credit_union_by_address(address, radius = 100)
-      self.class.post(query_endpoint, query: {
+      parse_response(self.class.post(query_endpoint, query: {
         address: address,
         type: 'address',
-        radius: radius.to_s })
+        radius: radius.to_s }))
     end
 
     def find_credit_union_by_name(name)
-      self.class.post(query_endpoint, query: {
+      parse_response(self.class.post(query_endpoint, query: {
         address: name,
-        type: 'cuname' })
+        type: 'cuname' }))
     end
 
     def find_credit_union_by_charter_number(charter_number)
-      self.class.post(query_endpoint, query: {
+      parse_response(self.class.post(query_endpoint, query: {
         radius: 100,
         address: charter_number,
-        type: 'cunumber' })
+        type: 'cunumber' }))
     end
 
     private
 
     def query_endpoint
       '/findCUByRadius.aspx'
+    end
+
+    def parse_response(response)
+      JSON.parse(response.body)
+    rescue JSON::ParserError
+      {}
     end
   end
 end
--- spec/ncua/client_spec.rb
+++ spec/ncua/client_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe NCUA::Client do
-
+  describe "parsing keys" do
+    context "Given an empty body" do
+      it "returns an empty array" do
+        mock_response = double(body: "")
+        allow(NCUA::Client).to receive(:post) { mock_response }
+        response = described_class.new.find_credit_union_by_charter_number("FOO")
+        expect(response.keys).to eq([])
+      end
+    end
+  end
 end
